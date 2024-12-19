0d9c296cc6c618b1b6574752dd8b250eed1ee4e7
yelim24 <yelimkim@hufs.ac.kr>
2024-12-19 14:35:13 +0900

    chromadb file access test

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import streamlit as st
 import json
-from openai import OpenAI
 from google.cloud import firestore
 from google.oauth2 import service_account
+
+from streamlit_chromadb_connection.chromadb_connection import ChromadbConnection
+from openai import OpenAI
 
 st.markdown("#### 채팅 테스트")
 st.write("채팅 테스트")
@@ -21,3 +23,45 @@ doc = doc_ref.get()
 # Let's see what we got!
 st.write("The id is: ", doc.id)
 st.write("The contents are: ", doc.to_dict())
+
+
+# Chroma DB에서 상담 시나리오 작성
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import Chroma
+
+def return_counseling_sinario(user_input, k=3):
+    """
+    상담 시나리오를 반환하는 함수.
+
+    Parameters:
+        user_input (str): 사용자 입력.
+        k (int, optional): 검색할 문서의 개수. 기본값은 3.
+    
+    Returns:
+        str: 검색 결과.
+    """
+    embedding_model_name = "snunlp/KR-SBERT-V40K-klueNLI-augSTS"
+    embeddings_model = HuggingFaceEmbeddings(
+                        model_name=embedding_model_name,
+                        model_kwargs={'device':"cpu"},
+                        encode_kwargs={'normalize_embeddings':True})
+
+    collection_name = "question_answer2"
+    db_dir = "./db/chromadb"
+
+    # Chroma DB 로드
+    vector_db = Chroma(
+        persist_directory=db_dir,
+        embedding_function=embeddings_model,
+        collection_name=collection_name)
+    
+    retriever = vector_db.as_retriever(search_kwargs={"k": k})
+    
+    docs = retriever.invoke(user_input)
+    
+    results = [f"내담자: {doc.page_content}\n상담사: {doc.metadata['answer']}"for doc in docs]
+    
+    return "\n\n".join(results)
+
+
+st.write(return_counseling_sinario("애들이 불쌍해"))
