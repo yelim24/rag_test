47628544d821dcb10e02c9c6ef8471bfb68fe79c
yelim24 <yelimkim@hufs.ac.kr>
2025-01-14 09:38:25 +0900

    Refactor import statement in llm_utils.py to use updated Chroma module from langchain.vectorstores

--- utils/llm_utils.py
+++ utils/llm_utils.py
@@ -2,7 +2,7 @@ import streamlit as st
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_openai import ChatOpenAI
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_chroma import Chroma
+from langchain.vectorstores import Chroma
 from langchain_core.runnables import RunnableWithMessageHistory, ConfigurableFieldSpec
 from utils.firestore_utils import get_session_history
 from utils.constants import PROMPT_TEMPLATE
